dcec63689003852ea92a53d0288b3904acf90d31
brainarrest <brainarrest@users.noreply.github.com>
2026-02-12 10:49:21 +0000

    docs: add usage example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Client } from './client';
 export { Resource, ListOptions, PaginatedResponse } from './types';
 export { SDKError, AuthError, RateLimitError } from './errors';
+// touched 2026-02-12
